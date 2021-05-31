a3c4aa2cef32eac3f921da37504e3850d4099901
Ondřej Tůma <ondr@centrum.cz>
2021-05-31 20:33:37 +0200

    updated section MyClients

--- src/components/section/MyClients.jsx
+++ src/components/section/MyClients.jsx
@@ -5,14 +5,26 @@ import Heading from '@components/layout/Heading'
 import Ribbon from '@components/custom/Ribbon'
 import Wrapper from '@components/layout/Wrapper'
 
+const clients = [
+  { alt: 'Dovolenka.sme.sk', url: '/img/clients/dovolenka.sme.sk.png' },
+  { alt: 'Marketing Miner', url: '/img/clients/mm.png' },
+  { alt: 'Northern Hikes', url: '/img/clients/nh.png' },
+  { alt: 'SEO zraz', url: '/img/clients/seozraz.png' },
+  { alt: 'Spotibo', url: '/img/clients/spotibo.png' },
+  { alt: 'Terraz', url: '/img/clients/terraz.png' },
+]
+
 export default function MyClients() {
   return (
     <Wrapper isSecondary isFat>
       <Container className={'text-center'}>
-        <Heading className={'mb-12 lg:mx-16'} ribbon={<Ribbon isWhite/>}>S textami som pomáhala aj týmto klientom</Heading>
+        <Heading className={'mb-12 lg:mx-16'} ribbon={<Ribbon isWhite/>}>S textami som pomáhala aj týmto
+          klientom</Heading>
         <div className="grid gap-10 mb-5 lg:grid-cols-4 lg:gap-x-20">
-          {Array(8).fill(0).map((_, i) => (
-            <div key={i} className={'uppercase text-3xl'}>Logo</div>
+          {clients.map(({ alt, url }) => (
+            <figure key={url}>
+              <img src={url} alt={alt}/>
+            </figure>
           ))}
         </div>
       </Container>
