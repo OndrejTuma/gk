2513caf84bd7a44e7b4cb46ce4c6ebaf0fd547bf
Ondřej Tůma <ondr@centrum.cz>
2021-05-31 20:36:39 +0200

    updated section MyReferences

--- src/components/section/MyReferences.jsx
+++ src/components/section/MyReferences.jsx
@@ -12,19 +12,14 @@ export default function MyReferences() {
         <Heading className={'mb-4'}>Títo ľudia ma odporúčajú ďalej</Heading>
         <div className="grid gap-4 lg:grid-cols-3 lg:gap-x-20">
           <Reference
-            imgUrl={'/img/placeholders/reference.jpg'}
-            name={'Filip z Marketing'}
-            description={'V online marketingu sa pohybujem už viac než 8 rokov, z toho 5 pôsobím na voľnej nohe. V minulosti som sa venovala predovšetkým link buildingu a obsahovému marketingu. Dnes mi je už bližšie samotné písanie.'}
+            imgUrl={'/img/reference/filip.jpg'}
+            name={'Filip Podstavec'}
+            description={'Těžko se hledají lidé, kteří vědí co dělají a zároveň jsou nadšení pro svou práci. Gabka splňovala oboje a díky tomu nám pomohla nastartovat práce na obsahu a texty na webu. Bez ní bychom v Marketing Mineru asi ještě stále byly bez blogu a bez srozumitelné databáze marketingových návodů a postupů.'}
           />
           <Reference
-            imgUrl={'/img/placeholders/reference.jpg'}
-            name={'Janko zo Spotibo'}
-            description={'V online marketingu sa pohybujem už viac než 8 rokov, z toho 5 pôsobím na voľnej nohe. V minulosti som sa venovala predovšetkým link buildingu a obsahovému marketingu. Dnes mi je už bližšie samotné písanie.'}
-          />
-          <Reference
-            imgUrl={'/img/placeholders/reference.jpg'}
-            name={'Nomádka Iwka'}
-            description={'V online marketingu sa pohybujem už viac než 8 rokov, z toho 5 pôsobím na voľnej nohe. V minulosti som sa venovala predovšetkým link buildingu a obsahovému marketingu. Dnes mi je už bližšie samotné písanie.'}
+            imgUrl={'/img/reference/ivka.jpg'}
+            name={'Ivka Grešlíkova'}
+            description={'Gabka je jedna veľká profíčka, ktorá dokáže ľudskou rečou vysvetliť, prečo niektoré veci na čitateľa a zákazníka nefungujú a ako to napraviť. Stála po mojej pravej ruke pri editovaní 250-stránkovej cestopisnej knihy U vás zjem všetko. Bez jej navigácie ako upraviť niektoré pasáže by kniha nebola ono.'}
           />
         </div>
       </Wrapper>
