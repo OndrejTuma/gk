55d6f9f2db3a2ef2907d56223b6373a42d9dafdf
Ondřej Tůma <ondr@centrum.cz>
2021-06-01 09:25:04 +0200

    text align custom in MyQuality

--- src/components/custom/MyQuality.jsx
+++ src/components/custom/MyQuality.jsx
@@ -7,7 +7,10 @@ import RoundImage from '@components/custom/RoundImage'
 
 export default function MyQuality({ imgUrl, name, description, isInverted, ribbon = <Ribbon level={2} isWhite/> }) {
   return (
-    <div className={cn('text-center lg:text-left lg:flex mb-10 lg:mb-2', { 'lg:text-right': isInverted })}>
+    <div className={cn('text-center lg:flex mb-10 lg:mb-2', {
+      'lg:text-right': isInverted,
+      'lg:text-left': !isInverted,
+    })}>
       <div className={cn('mt-10', {
         'lg:ml-20 order-2': isInverted,
         'lg:mr-20 order-1': !isInverted,
